21884683ff731d4b9a53f1aa99f79a1b10082961
Ever <everstarckone@protonmail.com>
2022-06-13 21:35:41 -0500

    feat(action): add new code xd

--- dist/index.js
+++ dist/index.js
@@ -8863,18 +8863,87 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2438);
 const github = __nccwpck_require__(7960);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('owner');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+const main = async () => {
+    try {
+
+        console.log("Hello World!");
+        /**
+         * We need to fetch all the inputs that were provided to our action
+         * and store them in variables for us to use.
+         **/
+        const token = core.getInput('github-token', { required: true });
+        const owner = core.getInput('owner', { required: true });
+        const repo = core.getInput('repo', { required: true });
+        const pull_number = core.getInput('pull_number', { required: true });
+
+        /**
+         * Now we need to create an instance of Octokit which will use to call
+         * GitHub's REST API endpoints.
+         * We will pass the token as an argument to the constructor. This token
+         * will be used to authenticate our requests.
+         * You can find all the information about how to use Octokit here:
+         * https://octokit.github.io/rest.js/v18
+         **/
+        const octokit = new github.getOctokit(token);
+
+        console.log("Hello World! 2");
+
+        /**
+         * We need to fetch the list of files that were changes in the Pull Request
+         * and store them in a variable.
+         * We use octokit.paginate() to automatically loop over all the pages of the
+         * results.
+         * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+         */
+        const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+            owner,
+            repo,
+            pull_number: pull_number,
+        });
+
+        console.log(changedFiles);
+        
+        console.log("BRRR")
+        
+        const prNumber = github.context.payload.pull_request.number;
+        const xd = JSON.stringify(github.context.payload)
+
+        console.log(xd, "Xd")
+        console.log(github)
+        console.log({ prNumber })
+
+        /**
+         * Contains the sum of all the additions, deletions, and changes
+         * in all the files in the Pull Request.
+         **/
+        let diffData = {
+            additions: 0,
+            deletions: 0,
+            changes: 0
+        };
+
+        // Reference for how to use Array.reduce():
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+        diffData = changedFiles.reduce((acc, file) => {
+            acc.additions += file.additions;
+            acc.deletions += file.deletions;
+            acc.changes += file.changes;
+            return acc;
+        }, diffData);
+
+        console.log(diffData)
+
+        core.setOutput("pull-request-body", "yeyeyeyeyye");
+    } catch (error) {
+        console.log("error")
+        core.setFailed(error.message);
+    }
 }
+
+// Call the main function to run the action
+main();
+
 })();
 
 module.exports = __webpack_exports__;
