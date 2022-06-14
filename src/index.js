b6a2a9a379cf065e6b86a435b9753819f2851583
Ever <everstarckone@protonmail.com>
2022-06-13 22:04:31 -0500

    feat(action): get list of commits

--- src/index.js
+++ src/index.js
@@ -1,67 +1,54 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 const main = async () => {
-    try {
+  try {
+    console.log("Hello World!");
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     **/
+    const token = core.getInput("github-token", { required: true });
+    const owner = core.getInput("owner", { required: true });
+    const repo = core.getInput("repo", { required: true });
+    const pull_number = core.getInput("pull_number", { required: true });
 
-        console.log("Hello World!");
-        /**
-         * We need to fetch all the inputs that were provided to our action
-         * and store them in variables for us to use.
-         **/
-        const token = core.getInput('github-token', { required: true });
-        const owner = core.getInput('owner', { required: true });
-        const repo = core.getInput('repo', { required: true });
-        const pull_number = core.getInput('pull_number', { required: true });
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * GitHub's REST API endpoints.
+     * We will pass the token as an argument to the constructor. This token
+     * will be used to authenticate our requests.
+     * You can find all the information about how to use Octokit here:
+     * https://octokit.github.io/rest.js/v18
+     **/
+    const octokit = new github.getOctokit(token);
 
-        /**
-         * Now we need to create an instance of Octokit which will use to call
-         * GitHub's REST API endpoints.
-         * We will pass the token as an argument to the constructor. This token
-         * will be used to authenticate our requests.
-         * You can find all the information about how to use Octokit here:
-         * https://octokit.github.io/rest.js/v18
-         **/
-        const octokit = new github.getOctokit(token);
+    console.log(owner, "owner");
+    console.log(repo, "repo");
+    console.log(pull_number, "pull_number");
 
-        console.log(owner, "owner");
-        console.log(repo, "repo");
-        console.log(pull_number, "pull_number");
+    console.log("Hello World! 2");
 
-        console.log("Hello World! 2");
+    /**
+     * We need to fetch the list of files that were changes in the Pull Request
+     * and store them in a variable.
+     * We use octokit.paginate() to automatically loop over all the pages of the
+     * results.
+     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+     */
 
-        /**
-         * We need to fetch the list of files that were changes in the Pull Request
-         * and store them in a variable.
-         * We use octokit.paginate() to automatically loop over all the pages of the
-         * results.
-         * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-         */
-
-        console.log("BRRR")
-
-        console.log(github)
-        const xd = JSON.stringify(github.context.payload)
-        console.log(xd, "Xd")
-        const prNumber = github.context.payload.pull_request.number;
-        console.log({ prNumber })
+    // octokit get List commits on a pull request
+    const commits = await octokit.pulls.listCommits(repo, pull_number);
+    console.log(typeof commits, "commits type");
+    console.log(commits, "commits");
 
 
-        const { data: changedFiles } = await octokit.rest.pulls.listFiles({
-            owner,
-            repo,
-            pull_number: pull_number,
-        });
-
-        console.log(changedFiles)
-
-        core.setOutput("pull-request-body", "yeyeyeyeyye");
-    } catch (error) {
-        console.log("error")
-        core.setFailed(error.message);
-    }
-}
+    core.setOutput("pull-request-body", "yeyeyeyeyye");
+  } catch (error) {
+    console.log("error");
+    core.setFailed(error.message);
+  }
+};
 
 // Call the main function to run the action
 main();
